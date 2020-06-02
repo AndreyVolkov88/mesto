04cc4838d9d1888a93a40da4f3c750ada57d79b3
Andrey Volkov <fullon1988@gmail.com>
2020-06-03 01:09:12 +0300

    Добавление Фотографий

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
       name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+      link: 'https://turgeek.ru/upload/tour/images_more/6393/big-4-trekking-v-arhyze-komfort-tur-s-gostinicej.jpg'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+      name: 'Казань',
+      link: 'https://avatars.mds.yandex.net/get-zen_doc/51081/pub_5c0f925ad60cb600aae80ce4_5c0f94eb46ef5c00aaa819e6/scale_1200'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+      name: 'Рязань',
+      link: 'https://www.onetwotrip.com/ru/blog/wp-content/uploads/2018/02/ryazan-kremlin.jpg'
   },
   {
       name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+      link: 'https://avatars.mds.yandex.net/get-pdb/1789050/6613b2fe-3409-437f-8c30-28c8ed76f7a8/s1200?webp=false'
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+      name: 'Кубань',
+      link: 'https://avatars.mds.yandex.net/get-pdb/245485/bb6af271-5c29-4628-959b-2787246068f8/s1200?webp=false'
   },
   {
       name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+      link: 'https://avatars.mds.yandex.net/get-pdb/1748217/bdbcce6e-2f47-4f69-8e21-34137b716576/s1200?webp=false'
   }
 ];
 
